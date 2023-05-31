a03b64a4826468f9ea14da8114f0d4c5d6d27e49
BeomSu <48756063+Seobeomsu@users.noreply.github.com>
2023-05-31 23:24:04 +0900

    Feat : Map

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LegionSelectbox from '../components/LegionSelectbox'
 import LeisureSelectbox from '../components/LeisureSelectbox'
 import { useNavigate } from 'react-router-dom'
-
+import Map from '../components/Map'
 export default function Home() {
   const Navigate = useNavigate();
 
@@ -16,6 +16,7 @@ export default function Home() {
   return (
     <>
     <div>Home</div>
+    <Map/>
     <LegionSelectbox></LegionSelectbox>
     <LeisureSelectbox></LeisureSelectbox>
     <button onClick={GoodDayConvert}>놀러가기 좋은날~</button>
