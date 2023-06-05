d5c8fc5acf31b4c4d22c85a351c6ea2adb0bb2b3
BeomSu <48756063+Seobeomsu@users.noreply.github.com>
2023-06-06 03:03:53 +0900

    Design : PlaceBox

--- frontend/src/components/PlaceBox.jsx
+++ frontend/src/components/PlaceBox.jsx
@@ -3,8 +3,14 @@ import styled from 'styled-components'
 
 const Wrap = styled.div`
   background-color: aqua;
-  width: 200px;
-  height: 200px;
+  display: block;
+  flex-shrink: 0;
+  width: 600px;
+  height: 450px;
+  margin: auto 20px auto 20px;
+  border-radius: 30px;
+  padding: 10px;
+  
 `
 
 export default function PlaceBox({name,address,context,imgaddress}) {
