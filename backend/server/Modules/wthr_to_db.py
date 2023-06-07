cf532371b598686c7365f27d313569bcc093be8b
AhnJewon <inno6734@gmail.com>
2023-06-07 12:05:04 +0900

    add_explanation

--- backend/server/Modules/wthr_to_db.py
+++ backend/server/Modules/wthr_to_db.py
@@ -8,16 +8,18 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 #APScheduler background 실행시 uwsgi의 multi threads 활설화 필요
 
-
+#scheduler 실행여부 확인용
 def test():
     print("Scheduler is alive!")
 
+#data mysql저장 실행
 def getWthrData():
     print("공공데이터 ASOS API불러오고 저장실행.")
     date = datetime.now()
     temp = ASOS(date, 131)
     temp.asosdata()
     
+#scheduler 실행    
 def start_wthr_to_db():    
     schedule = BackgroundScheduler(daemon=True, timezone='Asia/Seoul')
     schedule.add_job(test, 'interval', minutes=31, id='test')
