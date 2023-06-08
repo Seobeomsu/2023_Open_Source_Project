574377a3d0b4c518dbef6bdbb519a645bcffb3eb
AhnJewon <inno6734@gmail.com>
2023-06-08 18:55:28 +0900

    update_connection_way

--- backend/server/Modules/connDB.py
+++ backend/server/Modules/connDB.py
@@ -5,15 +5,18 @@ from config.conf import mysqlConf
 import pymysql
 from sqlalchemy import create_engine
 
+#mysql DB에 연결
 host=mysqlConf['host']
 user=mysqlConf['user']
 passwd=mysqlConf['passwd']
 db=mysqlConf['db']
 
+#sqlalchemy의 연결 방식 사용
 class alchemy:
-    db_connection_str = 'mysql+pymysql://{0}:{1}@{2}/{3}?charset=utf8'.format(user,passwd,host,db)
+    db_connection_str = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(user,passwd,host,db)
     db_connection = create_engine(db_connection_str)
     conn = db_connection.connect()
 
+#pymysql의 연결방식 사용
 class mysql:
     conn = pymysql.connect(host=mysqlConf['host'],user=mysqlConf['user'],password=mysqlConf['passwd'],db=mysqlConf['db'],charset='utf8mb4')
