a816c0d6636b471069a689506e28f17e374c402c
AhnJewon <inno6734@gmail.com>
2023-06-10 16:44:27 +0900

    create_mysql_DB&user

--- backend/server/src/set_up_mysql.py
+++ backend/server/src/set_up_mysql.py
@@ -3,13 +3,16 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from config.conf import mysqlConf
+db = mysqlConf['db']
+user = mysqlConf['user']
+host = mysqlConf['host']
+passwd = mysqlConf['passwd']
 
-os.system('sudo mysql -u root -p')
-os.system('\n')
-os.system(f"create database {mysqlConf['db']} default character set utf8;")
-os.system(f"create user '{mysqlConf['user']}'@'{mysqlConf['host']}' identified by '{mysqlConf['passwd']}';")
-os.system(f"grant all privileges on *.* to '{mysqlConf['user']}'@'{mysqlConf['host']}';")
-os.system('exit')
+
+os.system('')
+os.system(f"sudo mysql -u root create database {db} default character set utf8;")
+os.system(f"sudo mysql -u root create user '{user}'@'{host}' identified by '{passwd}';")
+os.system(f"sudo mysql -u root grant all privileges on *.* to '{user}'@'{host}';")
 os.system('sudo cp ./server/DB/SURFACE_ASOS_131_DAY_2012_2022.csv /var/lib/mysql')
 
 print('MYSQL 사용자 생성완료')
