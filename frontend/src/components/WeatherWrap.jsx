4f7d346b98c0b1ed8e338b9f702f734c9e646308
BeomSu <48756063+Seobeomsu@users.noreply.github.com>
2023-06-10 23:08:58 +0900

    Feature : WeatherWrap horizontal sroll

--- frontend/src/components/WeatherWrap.jsx
+++ frontend/src/components/WeatherWrap.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import WeatherBox from '../components/WeatherBox'
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Wrap = styled.div`
-  display: flex;
+  display: block;
   height: 350px;
+  width: 1440px;
   flex-shrink: 0;
   overflow: auto;
   white-space: nowrap;
+  .slick-slider{
+    display: flex;
+    width: auto;
+    overflow: visible;
+  }
+  .slick-list{ //얘로 크기조정 했음
+    display: block;
+    left: -500px;
+    width: 350px;
+    height: 350px;
+    margin: 0 auto;
+    overflow: visible;
+  }
+  .slick-slide{
+    //width: 350px !important;
+  }
+  .slick-arrow{
+    font-size: 40px
+  }
+  .slick-prev, .slick-next{ //얘는 양옆 버튼. 커스텀 해줘야 보임
+    z-index: -1000;
+    	font-family: 'slick';
+        font-size: 40px;
+        line-height: 1;
+        opacity: .75;
+        color: #000000;
+        -webkit-font-smoothing: antialiased;
+    }
 `
 
 const weatherdata = [ // 총 10개
@@ -26,8 +57,18 @@ const weatherdata = [ // 총 10개
 
 export default function WeatherWrap() {
   // const WeatherData = JSON.parse(localStorage.getItem('WeatherData')); // 데이터 로드
+
+  const settings = {
+    dots: false,
+    Infinity: true,
+    speed: 300,
+    slidesToshow : 4,
+    slidesToScroll: 1,
+    Arrow : true
+  };
   return (
     <Wrap>
+      <Slider {...settings} >
         {weatherdata.map((item)=>(
           <WeatherBox
           key={item.date}
@@ -38,52 +79,7 @@ export default function WeatherWrap() {
           state = {item.state}
         />
       ))}
+      </Slider>
     </Wrap>
   )
-}
-
-function LeftArrow() {
-  const { isFirstItemVisible, scrollPrev } =
-    React.useContext(VisibilityContext);
-
-  return (
-    <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-      Left
-    </Arrow>
-  );
-}
-
-function RightArrow() {
-  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
-
-  return (
-    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-      Right
-    </Arrow>
-  );
-}
-
-function Card({ onClick, selected, title, itemId }) {
-  const visibility = React.useContext(VisibilityContext);
-
-  return (
-    <div
-      onClick={() => onClick(visibility)}
-      style={{
-        width: '160px',
-      }}
-      tabIndex={0}
-    >
-      <div className="card">
-        <div>{title}</div>
-        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
-        <div>selected: {JSON.stringify(!!selected)}</div>
-      </div>
-      <div
-        style={{
-          height: '200px',
-        }}
-      />
-    </div>
-  );
 }
