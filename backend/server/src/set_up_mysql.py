c9320582ae1a0209e0419c0d7e4371bda2a39eee
AhnJewon <inno6734@gmail.com>
2023-06-10 16:52:37 +0900

    update_code

--- backend/server/src/set_up_mysql.py
+++ backend/server/src/set_up_mysql.py
@@ -10,9 +10,9 @@ passwd = mysqlConf['passwd']
 
 
 os.system('')
-os.system(f"sudo mysql -u root create database {db} default character set utf8;")
-os.system(f"sudo mysql -u root create user '{user}'@'{host}' identified by '{passwd}';")
-os.system(f"sudo mysql -u root grant all privileges on *.* to '{user}'@'{host}';")
+os.system(f"sudo mysqladmin -u root create database {db} default character set utf8;")
+os.system(f"sudo mysqladmin -u root create user '{user}'@'{host}' identified by '{passwd}';")
+os.system(f"sudo mysqladmin -u root grant all privileges on *.* to '{user}'@'{host}';")
 os.system('sudo cp ./server/DB/SURFACE_ASOS_131_DAY_2012_2022.csv /var/lib/mysql')
 
 print('MYSQL 사용자 생성완료')
